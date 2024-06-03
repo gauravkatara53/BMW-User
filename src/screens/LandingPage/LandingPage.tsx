2e0226aadd02113e1dee014a6129889d9ec6c7e6
unknown <xlongclaw@gmail.com>
2024-06-03 20:14:58 +0530

    feat(ready-to-sell-section): develop base ui

--- src/screens/LandingPage/LandingPage.tsx
+++ src/screens/LandingPage/LandingPage.tsx
@@ -1,11 +1,13 @@
 import Hero from "./containers/Hero";
 import OurRecommendation from "./containers/OurRecommendation";
+import ReadyToSell from "./containers/ReadyToSell";
 
 export default function LandingPage() {
   return (
     <div className="flex flex-col gap-32">
-      <Hero/>
-      <OurRecommendation/>
+      <Hero />
+      <OurRecommendation />
+      <ReadyToSell />
     </div>
   );
 }
