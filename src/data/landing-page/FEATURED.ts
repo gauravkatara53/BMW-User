56f4f2ce4f017e4053c6ad7d214193203dd92a70
unknown <xlongclaw@gmail.com>
2024-06-04 01:31:37 +0530

    chore: add dummy data to recommendation section

---
+++ src/data/landing-page/FEATURED.ts
@@ -0,0 +1,38 @@
+export const FEATURED = [
+    {
+        img:"dummy-featured1.png",
+        propertyName:"Highlands Warehouse",
+        price:"35000000",
+        ownerImage:"dummy-user-img-1.png",
+        ownerName:"Envy Oliver",
+        city:"New Delhi",
+        tag:"popular"
+    },
+    {
+        img:"dummy-featured2.png",
+        propertyName:"Woodland Warehouse",
+        price:"45000000",
+        ownerImage:"dummy-user-img-2.png",
+        ownerName:"Cody Rhodes",
+        city:"Bhopal, Madhya Pradesh",
+        tag:"popular"
+    },
+    {
+        img:"dummy-featured3.png",
+        propertyName:"Woodland Warehouse",
+        price:"80000000",
+        ownerImage:"dummy-user-img-3.png",
+        ownerName:"James Smith",
+        city:"Pune, Maharashtra",
+        tag:"popular"
+    },
+    {
+        img:"dummy-featured4.png",
+        propertyName:"Woodland Warehouse",
+        price:"80000000",
+        ownerImage:"dummy-user-img-4.png",
+        ownerName:"Drake",
+        city:"Pune, Maharashtra",
+        tag:"popular"
+    },
+]
