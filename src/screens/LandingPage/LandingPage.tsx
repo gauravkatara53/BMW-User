c3e37f94e4b1f622a209bed60bad234a5ac993d3
unknown <xlongclaw@gmail.com>
2024-06-04 00:40:31 +0530

    feat(our-reviews): develop base ui

--- src/screens/LandingPage/LandingPage.tsx
+++ src/screens/LandingPage/LandingPage.tsx
@@ -4,13 +4,15 @@ import OurRecommendation from "./containers/OurRecommendation";
 import ReadyToSell from "./containers/ReadyToSell";
 import Newsletter from "./containers/Newsletter";
 import ArticleSection from "./containers/ArticlesSection";
+import OurReviews from "./containers/OurReviews";
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col gap-40">
+    <div className="flex flex-col gap-40 overflow-x-hidden">
       <Hero />
       <OurRecommendation />
       <ReadyToSell />
+      <OurReviews/>
       <ArticleSection/>
       <Newsletter/>
       <WHFooter/>
