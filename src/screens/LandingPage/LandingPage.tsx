c2d827cac3981c14d01e18f6a5b51748b77f14be
unknown <xlongclaw@gmail.com>
2024-06-05 11:49:53 +0530

    feat(lp): cleared footer component

--- src/screens/LandingPage/LandingPage.tsx
+++ src/screens/LandingPage/LandingPage.tsx
@@ -1,24 +1,24 @@
 import WHFooter from "@/components/common/WHFooter";
-import Hero from "./containers/Hero";
-import OurRecommendation from "./containers/OurRecommendation";
-import ReadyToSell from "./containers/ReadyToSell";
-import Newsletter from "./containers/Newsletter";
-import ArticleSection from "./containers/ArticlesSection";
-import OurReviews from "./containers/OurReviews";
+import Hero from "./sections/Hero";
+import OurRecommendation from "./sections/OurRecommendation";
+import ReadyToSell from "./sections/ReadyToSell";
+import Newsletter from "./sections/Newsletter";
+import ArticleSection from "./sections/ArticlesSection";
+import OurReviews from "./sections/OurReviews";
 import RecommendationProvider from "@/providers/RecommendationProvider";
 
 export default function LandingPage() {
   return (
     <div className="flex flex-col gap-16 sm:gap-40 overflow-x-hidden">
-      {/* <Hero />
+      <Hero />
       <RecommendationProvider>
         <OurRecommendation />
-      </RecommendationProvider> */}
+      </RecommendationProvider>
       <ReadyToSell />
       <OurReviews />
-      {/* <ArticleSection /> */}
-      {/* <Newsletter /> */}
-      {/* <WHFooter /> */}
+      <ArticleSection />
+      <Newsletter />
+      <WHFooter />
     </div>
   );
 }
