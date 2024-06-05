12bc79aec12a6ca03162619953e9accfecaebf6f
unknown <xlongclaw@gmail.com>
2024-06-05 21:39:01 +0530

    fix: ui bugs and add framer anims

--- src/components/common/WHNavLink.tsx
+++ src/components/common/WHNavLink.tsx
@@ -7,7 +7,7 @@ export default function WHNavLink({
   title = "No Link Title",
 }: TProps): React.JSX.Element {
   return (
-    <a href="#" className="text-light-white-F0 bg-white/10 py-[6px] px-4 text-sm xl:text-base rounded-full border border-white/30">
+    <a href="#" className="text-light-white-F0 bg-white/10 py-[6px] px-4 text-sm xl:text-base rounded-full border border-white/30 hover:border-WH-light-green hover:text-WH-light-green">
       {title}
     </a>
   );
