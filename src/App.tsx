8f41a55a4b1ab4a30793234ac1699be669bd765c
Aman <amankumar94719@gmail.com>
2024-06-06 14:23:37 +0530

    Naming Change WHNotFoundPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from "@/screens/LandingPage";
-import NotFoundPage from './components/common/NotFoundPage';
+import NotFoundPage from './components/common/WHNotFoundPage';
 
 export default function App() {
   return (
