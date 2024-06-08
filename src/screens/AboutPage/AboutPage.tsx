e0d9e4fa6df26760d71c28d5063557f23abd2d94
Aman <amankumar94719@gmail.com>
2024-06-08 19:41:08 +0530

    GeteverythingDone page

--- src/screens/AboutPage/AboutPage.tsx
+++ src/screens/AboutPage/AboutPage.tsx
@@ -1,9 +1,11 @@
 import WHFooter from "@/components/common/WHFooter";
 import NewsAndUpdates from "./sections/NewsAndUpdates";
+import GeteverythingDone from "./sections/GeteverythingDone";
 
 export default function AboutPage() {
   return (
     <div>
+      <GeteverythingDone />
       <NewsAndUpdates />
       <WHFooter />
     </div>
