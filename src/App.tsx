19b287b02de614e4e31017c458c4c6911321baa7
unknown <xlongclaw@gmail.com>
2024-06-10 16:38:27 +0530

    feat: add TestDeleteAccount screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NotFoundPage from "./components/common/WHNotFoundPage";
 import { AboutPage,LandingPage } from "@/screens";
+import TestDeleteAccount from "./screens/TestDeleteAccount";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/delete-account" element={<TestDeleteAccount/>} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
