8e873b03f43549b623f78b2ece3c24b352d25244
gauravkatara53 <gauravkatara53@gmail.com>
2024-08-20 21:56:02 +0530

    add navbar for small screan , subscribe section for small screen , footer updated

--- src/screens/AboutPage/sections/WhoWeAre.tsx
+++ src/screens/AboutPage/sections/WhoWeAre.tsx
@@ -6,7 +6,7 @@ export default function WhoWeAre() {
     <div className="flex flex-col items-center lg:px-32 md:px-16 sm:px-8 px-4 gap-8 mt-0">
       <APSectionHeading bgTitle="COMPANY" smallTitle="Who we are?" />
       <div className="w-full bg-WH-light-pink/30 rounded-[5rem] flex flex-col lg:flex-row lg:p-16">
-        <div className="max-w-[38rem] flex flex-col gap-8 lg:pr-16">
+        <div className="flex-1 flex flex-col gap-8 lg:pr-16">
           <p className="text-lg font-normal text-black/80">
             At Book My Warehouse, we provide flexible and secure warehouse
             solutions tailored to your business needs. Our tech-driven platform
@@ -18,40 +18,40 @@ export default function WhoWeAre() {
             in ensuring efficient inventory management, helping your business
             grow and thrive.
           </p>
-          <div className="lg:flex lg:items-center lg:justify-start">
-            <img src="quote.png" alt="" className="lg:mr-4" />
-            <p className="italic font-semibold text-2xl -mt-[1.5rem]">
+          <div className="flex items-center justify-center lg:justify-start mt-4 lg:mt-0">
+            <img src="quote.png" alt="" className="w-12 h-12 lg:mr-4" />
+            <p className="italic font-semibold text-xl lg:text-2xl text-center lg:text-left lg:-mt-[1.5rem]">
               Effortless Storage Solutions for Every Business
             </p>
           </div>
         </div>
 
-        <div className="flex-1 mx-0 lg:mx-16 mt-8 lg:mt-0 lg:relative">
-          <div className="lg:absolute lg:top-0 lg:left-0">
+        <div className="hidden lg:flex lg:flex-1 lg:relative lg:flex-row lg:items-center lg:justify-end">
+          <div className="lg:absolute lg:top-0 lg:right-1/2 lg:translate-x-1/2">
             <APAchievementCard
               imgUrl="article1.png"
               title="Developers"
               value="300+"
-              height={350}
-              width={350}
+              height={250}
+              width={250}
             />
           </div>
-          <div className="lg:absolute lg:top-[-5rem] lg:right-0">
+          <div className="lg:absolute lg:top-2/4 lg:right-0">
             <APAchievementCard
               imgUrl="article1.png"
-              title="Founded in"
+              title="Founded on"
               value="2011"
               width={250}
-              height={250}
+              height={200}
               gradient="TYPE-2"
             />
           </div>
-          <div className="lg:absolute lg:bottom-0 lg:right-0">
+          <div className="lg:absolute lg:bottom-3/4 lg:right-0">
             <APAchievementCard
               imgUrl="article1.png"
               title="Projects"
               value="1000+"
-              width={400}
+              width={250}
               height={200}
               gradient="TYPE-3"
             />
