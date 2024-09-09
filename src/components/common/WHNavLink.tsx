f31908c91887a6f886abcb6b080eff4a03c00008
gauravkatara53 <gauravkatara53@gmail.com>
2024-09-09 22:27:23 +0530

    create navbar according to new figma, hero ection also

--- src/components/common/WHNavLink.tsx
+++ src/components/common/WHNavLink.tsx
@@ -27,23 +27,25 @@ export default function WHNavLink({
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  const isSmallScreen = windowWidth < 640; // Define small screen width
+
   return (
     <Link
       to={to}
       className={cn(
-        "py-[6px] px-4 text-sm xl:text-base rounded-full border transition-colors duration-300 ease-in-out",
+        "py-[6px] px-4 text-sm xl:text-base rounded-full transition-colors duration-300 ease-in-out",
         {
-          // Dark theme styles
-          "text-black border-white hover:border-white hover:text-WH-light-green-01":
-            isDark && !(pathName === to),
-          "bg-white/10 border-white/30 hover:border-WH-light-green hover:text-WH-light-green":
-            !isDark,
-          // Mobile screen styles
-          "text-gray-800": !isDark && windowWidth < 640,
-          // Desktop screen styles
-          "text-light-white-F0": !isDark && windowWidth >= 1020,
-          // Active link styles
-          "border-black": pathName === to,
+          // Small screen styles (no border)
+          "bg-gray-100 text-black border-none hover:bg-gray-300":
+            isSmallScreen && !isDark,
+          "bg-gray-200 text-gray-900 border-none hover:bg-gray-400":
+            isSmallScreen && isDark,
+
+          // Larger screen styles (with border)
+          "text-black border-gray-400 bg-gray-200 hover:border-white hover:text-white hover:bg-gradient-to-b hover:from-[#674CEC] hover:to-[#8D77FC] border":
+            !isSmallScreen && isDark,
+          "bg-white/10 border-white/30 hover:border-WH-light-green hover:text-WH-light-green border":
+            !isSmallScreen && !isDark,
         }
       )}
     >
