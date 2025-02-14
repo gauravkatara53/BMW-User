5a60ace74dbfeaee3e10288a06ae23866eff602a
gauravkatara53 <gauravkatara53@gmail.com>
2025-02-14 16:58:29 +0530

    add

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://bmw-backend-l85a.onrender.com", // Your Render backend URL
+        changeOrigin: true,
+        secure: true, // Ensure HTTPS is used
+      },
     },
   },
   plugins: [react()],
